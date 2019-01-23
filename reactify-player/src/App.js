d71f7ff9d79ec86c1d097a503c8e8c3e5324e86f
keeganosler <oslerkeegan@gmail.com>
2019-01-23 14:05:54 -0700

    make buttons work

--- reactify-player/src/App.js
+++ reactify-player/src/App.js
@@ -46,6 +46,11 @@ class App extends Component {
                 <p>Artist: {artistName}</p>
                 <p>Song: {trackName}</p>
                 <p>Album: {albumName}</p>
+                <p>
+                    <button onClick={() => this.onPrevClick()}>Previous</button>
+                    <button onClick={() => this.onPlayClick()}>{playing ? "Pause" : "Play"}</button>
+                    <button onClick={() => this.onNextClick()}>Next</button>
+                </p>
               </div>)
               :
               (<div>
@@ -95,15 +100,48 @@ createEventHandlers() {
   this.player.on('account_error', e => { console.error(e); });
   this.player.on('playback_error', e => { console.error(e); });
 
-  // Playback status updates
-  this.player.on('player_state_changed', state => { console.log(state); });
-
-  // Ready
+  this.player.on('player_state_changed', state => this.onStateChanged(state));
   this.player.on('ready', data => {
     let { device_id } = data;
     console.log("Let the music play on!");
     this.setState({ deviceId: device_id });
   });
+}
+
+onStateChanged(state) {
+  if (state !== null) {
+    const {
+      current_track: currentTrack,
+      position,
+      duration,
+    } = state.track_window;
+    const trackName = currentTrack.name;
+    const albumName = currentTrack.album.name;
+    const artistName = currentTrack.artists
+      .map(artist => artist.name)
+      .join(", ");
+    const playing = !state.paused;
+    this.setState({
+      position,
+      duration,
+      trackName,
+      albumName,
+      artistName,
+      playing
+    });
+  }
+}
+
+onPrevClick(){
+    this.player.previousTrack();
+}
+
+onPlayClick(){
+    this.player.togglePlay();
+}
+
+onNextClick(){
+    this.player.nextTrack();
 }
 }
 
